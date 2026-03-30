6714e17f2c11c8e028118ae7d5e437a1b197b5e0
Chinki Raj <164533712+Chinki021@users.noreply.github.com>
2026-03-30 18:58:21 +0530

    Add SQL queries for product data retrieval
    Added multiple SQL queries to retrieve product information based on various conditions and formatting.

--- query.sql
+++ query.sql
@@ -1,1 +1,31 @@
 select * from products;
+
+
+--clauses test
+
+/* Q1. Show the name and price of all products.
+Q2. Show all products where the category is 'Electronics'.
+Q3. Group products by category. Show each category once.
+Q4. Show categories that have more than 1 product. (Use after
+GROUP BY)
+Q5. Show all products sorted by price in ascending order.
+Q6. Show only the first 3 products from the table.
+Q7. Show product name as "Item_Name" and price as
+"Item_Price".
+Q8. Show all the unique categories from the products table.*/
+
+select name,price from products;
+select * from products where category='Electronics';
+select category from products group by category;
+select category, count(*) from products
+group by category
+having count(*)>1;
+
+select * from products order by price;
+select * from products order by price DESC;
+
+select * from products limit 3;
+
+select name as item_name , price as item_price from products;
+
+select distinct category from products;
